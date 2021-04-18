9c8b2eea7c06d42d0b29a32bd8ce3d5ea68017b7
tduyan <55606258+tduyan@users.noreply.github.com>
2021-04-17 22:15:59 -0700

    Updated disabling specific sites

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        return { cancel : true }
+    },
+    { urls: [ "*://*.facebook.com/*"]},
+    ["blocking"]
+)
