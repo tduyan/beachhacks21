1eaa48799ee803299b1c5ca8ca8a72e02e790f3c
sooada <72992901+sooada@users.noreply.github.com>
2021-04-18 01:02:17 -0700

    idk

--- time.js
+++ time.js
@@ -1,3 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button1').addEventListener('click',
+    onclick, false)
+
+    function onclick() {
+        let btn = document.querySelector('item1'),
+        startTime = new Date();
+
+        if(btn){
+            btn.addEventListener('click',function() {
+                let miliSec = new Date() - startTime;
+                let elapsedTime = miliSec/1000;
+                document.write(elapsedTime + "Seconds.")
+            })
+            //document.write(elapsedTime)
+        } 
+    }
+        
+    }
+
 let init = function() {
     let btn = document.querySelector('item1'),
     startTime = new Date();
@@ -5,10 +25,13 @@ let init = function() {
     if(btn){
         btn.addEventListener('click',function() {
             let miliSec = new Date() - startTime;
-            console.log(miliSec/1000 + "Seconds.")
+            var elapsedTime = miliSec/1000;
+            write(elapsedTime + "Seconds.")
         })
     }
 }
+
+//document.addEventListener('click',function())
 
 document.addEventListener('DOMContentLoaded', function() {
     init();
