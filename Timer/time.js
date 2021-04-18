0334377737b0bb46a0baef4a5103a73e4bb29b6c
sooada <72992901+sooada@users.noreply.github.com>
2021-04-18 05:31:26 -0700

    renamed starter.js to time.js

--- Timer/time.js
+++ Timer/time.js
@@ -1,38 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button1').addEventListener('click',
-    onclick, false)
+document.getElementById("start").addEventListener("click",StartTime)
 
-    function onclick() {
-        let btn = document.querySelector('item1'),
-        startTime = new Date();
-
-        if(btn){
-            btn.addEventListener('click',function() {
-                let miliSec = new Date() - startTime;
-                let elapsedTimed = miliSec/1000;
-                document.write(elapsedTime + "Seconds.")
-            })
-            //document.write(elapsedTime)
-        } 
-    }
-        
-    }
-
-let init = function() {
-    let btn = document.querySelector('item1'),
-    startTime = new Date();
-
-    if(btn){
-        btn.addEventListener('click',function() {
-            let miliSec = new Date() - startTime;
-            var elapsedTime = miliSec/1000;
-            write(elapsedTime + "Seconds.")
-        })
-    }
+function StartTime(){
+   var starting = new Date().getTime();
+   //alert("You've started studying! Hurray!");
+   document.getElementById("startTimer").innerHTML = "You've started studying! Hurray!";
+   return{starting};
 }
 
-//document.addEventListener('click',function())
+document.getElementById("end").addEventListener("click",EndTime)
 
-document.addEventListener('DOMContentLoaded', function() {
-    init();
-},false);
+function EndTime(){
+   //let ending = Date();
+   //alert("You've started studying! Hurray!");
+   let miliSec = new Date().getTime() - StartTime;
+   let elapsedTime = miliSec/1000;
+   document.getElementById("endTimer").innerHTML = "Wow! You've studied for: " + elapsedTime.toString() + "seconds.";
+}
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('start').addEventListener('click',
+//     onclick, false)
+
+//     function onclick() {
+//         let startTime = new Date();
+//         chrome.tabs.query({currentWindow: true, active: true},
+//         function(tabs) {
+//             chrome.tabs.sendMessage(tabs[0].id, "You've started studying! Hurray!")
+//         })
+//     }
+// }, false)
