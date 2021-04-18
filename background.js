1e4e503a03e892d9390b15dd57b388a46f4a71c0
tduyan <55606258+tduyan@users.noreply.github.com>
2021-04-18 00:07:25 -0700

    Updated blocked sites
    added social media default filters

--- background.js
+++ background.js
@@ -1,7 +1,18 @@
+defaultFilters = [
+    "*://*.facebook.com/*",
+    "*://*.ask.fm/*",
+    "*://*.chess.com/*",
+    "*://*.crunchyroll.com/*",
+    "*://*.flickr.com/*",
+    "*://*.instagram.com/*",
+    "*://*.twitter.com/*",
+    "*://*.linkedin.com/*",
+]
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         return { cancel : true }
     },
-    { urls: [ "*://*.facebook.com/*"]},
+    { urls: defaultFilters },
     ["blocking"]
 )
