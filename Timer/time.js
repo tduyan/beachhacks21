c24874e7365a5632492af053286663aa115c64ca
sooada <72992901+sooada@users.noreply.github.com>
2021-04-18 08:59:57 -0700

    correct time fixed

--- Timer/time.js
+++ Timer/time.js
@@ -1,23 +1,43 @@
-document.getElementById("start").addEventListener("click",StartTime)
+
+{document.getElementById("start").addEventListener("click",combine)
 
 function StartTime(){
-   var startingDate = new Date();
-   var starting = startingDate.getTime();
+   const startingDate = new Date();
+   //var starting = startingDate.getTime();
    //alert("You've started studying! Hurray!");
    document.getElementById("startTimer").innerHTML = "You've started studying! Hurray!";
-   return{startingDate};
+
+   return startingDate;
+}};
+
+//document.getElementById("end").addEventListener("click",EndTime)
+function combine(){
+   var dateS = StartTime();
+   document.getElementById("end").addEventListener("click",function(){
+      var dateE = new Date();
+      //document.write(Date.parse(DateE.toString));
+      let miliSec = dateE.getTime() - dateS.getTime();
+      let elapsedTime = Math.round(miliSec/1000);
+      document.getElementById("endTimer").innerHTML = "Wow! <br/> You've studied for: " + elapsedTime.toString() + " seconds <br /> From: " + dateS.toUTCString() + "<br/> To: " + dateE.toUTCString();
+
+});
+   //var dateE = EndTime();
+   let miliSec = endingDate.getTime() - dateS.getTime();
+   //let elapsedTime = miliSec/1000;
+   //document.write(StartTime().toTimeString());
+   document.getElementById("endTimer").innerHTML = "Wow! You've studied for: " + miliSec.toString() + "seconds. From:" + dateS.toTimeString() + "to" + dateE.toTimeString();
 }
 
 document.getElementById("end").addEventListener("click",EndTime)
-
 function EndTime(){
    let endingDate = new Date();
    let ending = endingDate.getTime();
    //alert("You've started studying! Hurray!");
-   let miliSec = new Date().getTime() - StartTime.getTime;
-   let elapsedTime = miliSec/1000;
-   //document.write(new Date().getUTCMilliseconds());
-   document.getElementById("endTimer").innerHTML = "Wow! You've studied for: " + elapsedTime.toString() + "seconds. From:" + StartTime().toTimeString() + "to" + endingDate.toTimeString();
+   //let miliSec = endingDate.getTime() - date.getTime();
+   //let elapsedTime = miliSec/1000;
+   //document.write(StartTime().toTimeString());
+   //document.getElementById("endTimer").innerHTML = "Wow! You've studied for: " + miliSec.toString() + "seconds. From:" + date.toTimeString() + "to" + endingDate.toTimeString();
+   return endingDate;
 }
 
 function parseDate(dateString){
